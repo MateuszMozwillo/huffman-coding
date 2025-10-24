98e53f8d7251e19aca54ea3e2037eb930de0c794
mateusz <mateusz.mozwillo@gmail.com>
2025-10-24 20:25:45 +0200

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
 #include <iostream>
 #include <queue>
+#include <sstream>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 struct TreeNode {
     double propability;
+    string coded_word;
     string code;
 
     TreeNode *left;
     TreeNode *right;
 
-    TreeNode(double p, TreeNode *l = nullptr, TreeNode *r = nullptr) {
+    TreeNode(double p, string coded_word, TreeNode *l = nullptr, TreeNode *r = nullptr) {
         this->propability = p;
+        this->coded_word = coded_word;
         this->left = l;
         this->right = r;
         this->code = "";
@@ -26,7 +37,7 @@ struct TreeNode {
         }
     }
 
-    void encode_nodes(string current_code = "") {
+    void encode_nodes(const string& current_code = "") {
         this->code = current_code;
         if (this->left != nullptr) {
             this->left->encode_nodes(this->code + "0");
@@ -38,13 +49,25 @@ struct TreeNode {
 
     void print() {
         if (this->left == nullptr && this->right == nullptr) {
-            cout << "\npropability: " << this->propability << "\ncode: " << this->code << "\n";
+            cout << "\npropability: " << this->propability << "\ncoded value: " << this->coded_word << "\ncode: " << this->code << "\n";
         }
         if (this->left != nullptr) {
             this->left->print();
         }
         if (this->right != nullptr) {
             this->right->print();
+        }
+    }
+
+    void extract_leafs(vector<TreeNode*>& leafs) {
+        if (this->left == nullptr && this->right == nullptr) {
+            leafs.push_back(this);
+        }
+        if (this->left != nullptr) {
+            this->left->extract_leafs(leafs);
+        }
+        if (this->right != nullptr) {
+            this->right->extract_leafs(leafs);
         }
     }
 };
@@ -55,30 +78,86 @@ struct CompareTreeNodes {
     }
 };
 
-int main() {
+string load_text_from_file(const string& file_path) {
+    string line;
+    string text;
+    ifstream file(file_path);
+    while(getline(file, line)) {
+        text += line + "\n";
+    }
+    file.close();
+    return text;
+}
+
+vector<string> text_to_tokens(const string& to_tokenize) {
+    vector<string> result;
+
+    stringstream line_stream(to_tokenize);
+
+    string line;
+
+    while (getline(line_stream, line, '\n')) {
+        stringstream word_stream(line);
+        string word;
+        while (getline(word_stream, word, ' ')) {
+            result.push_back(word);
+            result.push_back(" ");
+        }
+        result.push_back("\n");
+    }
+    return result;
+}
+
+int main(int argc, const char* argv[]) {
+
+    unordered_map<string, uint64_t> map;
+
+    auto tokens = text_to_tokens(load_text_from_file("./test.txt"));
+    uint64_t word_count_sum = tokens.size();
+
+    for (string s : tokens) {
+
+        if (map.find(s) == map.end()) {
+            map[s] = 1;
+        } else {
+            map[s]++;
+        }
+    }
+
+    if (argc == 2) {
+        //compress
+        if (strcmp(argv[1], "-c")) {
+
+        }
+    }
 
     priority_queue<TreeNode*, vector<TreeNode*>, CompareTreeNodes> q;
 
-    q.push(new TreeNode(1./2.));
-    q.push(new TreeNode(1./4.));
-    q.push(new TreeNode(1./8.));
-    q.push(new TreeNode(1./16.));
-    q.push(new TreeNode(1./32.));
-    q.push(new TreeNode(1./32.));
+    for (auto const& [key, val] : map) {
+        q.push(new TreeNode((float)val/(float)word_count_sum, key));
+    }
 
     while (q.size() > 1) {
         TreeNode* temp1 = q.top();
         q.pop();
         TreeNode* temp2 = q.top();
         q.pop();
-        q.push(new TreeNode(temp1->propability + temp2->propability, temp1, temp2));
+        q.push(new TreeNode(temp1->propability + temp2->propability, "", temp1, temp2));
     }
 
     TreeNode* final = q.top();
     final->encode_nodes();
-    final->print();
+    // final->print();
 
+    unordered_map<string, string> codes;
+    vector<TreeNode*> leafs;
+    final->extract_leafs(leafs);
     delete final;
+    for (auto leaf : leafs) {
+        codes[leaf->coded_word] = leaf->code;
+    }
+
+
 
     return 0;
 }
