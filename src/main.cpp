b13a2f02bf7a2e212225d3c5cfd9edd89d8dbea1
mateusz <mateusz.mozwillo@gmail.com>
2025-10-29 18:33:39 +0100

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,123 +6,16 @@
 #include <iostream>
 #include <queue>
 #include <sstream>
+#include <string>
 #include <unordered_map>
 #include <vector>
 #include <bitset>
 
+#include "hoffman.hpp"
+
 using namespace std;
 
-struct TreeNode {
-    double propability;
-    string coded_word;
-    string code;
-
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode(double p, string coded_word, TreeNode *l = nullptr, TreeNode *r = nullptr) {
-        this->propability = p;
-        this->coded_word = coded_word;
-        this->left = l;
-        this->right = r;
-        this->code = "";
-    }
-
-    ~TreeNode() {
-        if (this->left != nullptr) {
-            delete this->left;
-        }
-        if (this->right != nullptr) {
-            delete this->right;
-        }
-    }
-
-    void encode_nodes(const string& current_code = "") {
-        this->code = current_code;
-        if (this->left != nullptr) {
-            this->left->encode_nodes(this->code + "0");
-        }
-        if (this->right != nullptr) {
-            this->right->encode_nodes(this->code + "1");
-        }
-    }
-
-    void print() {
-        if (this->left == nullptr && this->right == nullptr) {
-            cout << "\npropability: " << this->propability << "\ncoded value: " << this->coded_word << "\ncode: " << this->code << "\n";
-        }
-        if (this->left != nullptr) {
-            this->left->print();
-        }
-        if (this->right != nullptr) {
-            this->right->print();
-        }
-    }
-
-    void extract_leafs(vector<TreeNode*>& leafs) {
-        if (this->left == nullptr && this->right == nullptr) {
-            leafs.push_back(this);
-        }
-        if (this->left != nullptr) {
-            this->left->extract_leafs(leafs);
-        }
-        if (this->right != nullptr) {
-            this->right->extract_leafs(leafs);
-        }
-    }
-};
-
-struct CompareTreeNodes {
-    bool operator()(const TreeNode* a, const TreeNode* b) const {
-        return a->propability > b->propability;
-    }
-};
-
-string load_text_from_file(const string& file_path) {
-    string line;
-    string text;
-    ifstream file(file_path);
-    while(getline(file, line)) {
-        text += line + "\n";
-    }
-    file.close();
-    return text;
-}
-
-vector<string> text_to_tokens(const string& to_tokenize) {
-    vector<string> result;
-
-    stringstream line_stream(to_tokenize);
-
-    string line;
-
-    while (getline(line_stream, line, '\n')) {
-        stringstream word_stream(line);
-        string word;
-        while (getline(word_stream, word, ' ')) {
-            result.push_back(word);
-            result.push_back(" ");
-        }
-        result.push_back("\n");
-    }
-    return result;
-}
-
 int main(int argc, const char* argv[]) {
-
-    unordered_map<string, uint64_t> map;
-
-    auto tokens = text_to_tokens(load_text_from_file("./test.txt"));
-    uint64_t word_count_sum = tokens.size();
-
-    for (string s : tokens) {
-
-        if (map.find(s) == map.end()) {
-            map[s] = 1;
-        } else {
-            map[s]++;
-        }
-    }
 
     if (argc == 2) {
         //compress
@@ -134,62 +27,38 @@ int main(int argc, const char* argv[]) {
 
         }
     }
-
-    priority_queue<TreeNode*, vector<TreeNode*>, CompareTreeNodes> q;
-
-    for (auto const& [key, val] : map) {
-        q.push(new TreeNode((float)val/(float)word_count_sum, key));
-    }
-
-    while (q.size() > 1) {
-        TreeNode* temp1 = q.top();
-        q.pop();
-        TreeNode* temp2 = q.top();
-        q.pop();
-        q.push(new TreeNode(temp1->propability + temp2->propability, "", temp1, temp2));
-    }
-
-    TreeNode* final = q.top();
-    final->encode_nodes();
-    // final->print();
-
-    unordered_map<string, string> codes;
-    vector<TreeNode*> leafs;
-    final->extract_leafs(leafs);
-    for (auto const& leaf : leafs) {
-        codes[leaf->coded_word] = leaf->code;
-    }
-    delete final;
-
-    string map_as_string = "";
-    for (auto const& [key, val]: codes) {
-        map_as_string += key + ":" + val + '\n';
-    }
+    const size_t len = load_text_from_file("./test.txt").size();
+    std::vector<char> tokens = text_to_tokens(load_text_from_file("./test.txt"));
+    char chars[len];
+    auto dictionary = create_dictionary_from_tokens(tokens);
 
     string coded = "";
 
     for (auto const& token : tokens) {
-        coded += codes[token];
+        coded += dictionary[token];
     }
 
     string coded_as_bytes  = "";
-
+    int paded_bits = 0;
     for (size_t i = 0; i < coded.size(); i+=8) {
         string byte = coded.substr(i, 8);
         if (byte.size() < 8) {
-            byte.append(8 - byte.length(), '0');
+            paded_bits = 8 - byte.length();
+            byte.append(paded_bits, '0');
         }
         bitset<8> bits(byte);
         unsigned char byte_to_write = static_cast<unsigned char>(bits.to_ulong());
         coded_as_bytes.push_back(byte_to_write);
     }
 
+    auto map_as_string = dictionary_to_string(dictionary);
+
     ofstream output("coded.out");
     if (!output) {
-            std::cerr << "Error: Could not open file for writing!" << std::endl;
-            return 1;
+        cerr << "Error: Could not open file for writing!" << endl;
+        return 1;
     }
-    output << map_as_string.size() << '\n' <<map_as_string << '\n';
+    output << map_as_string.size() << '\n' << coded_as_bytes.size()  << '\n' << paded_bits << '\n' <<map_as_string << '\n';
     output.write(reinterpret_cast<const char*>(coded_as_bytes.data()),coded_as_bytes.size());
     // output.write(reinterpret_cast<const char*>(coded_as_bytes.data()), coded_as_bytes.size());
     output << coded_as_bytes;
